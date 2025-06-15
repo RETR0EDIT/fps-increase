79519c99f80ada4aa1eb6c40460e8d7acd66e4e3
RETR0EDIT <86560394+RETR0EDIT@users.noreply.github.com>
2025-06-15 12:04:32 +0200

    Update pluto.bat for bo1

--- plutonium.bat
+++ plutonium.bat
@@ -1,34 +1,15 @@
 @echo off
 setlocal
 
-
 set "source=%LocalAppdata%\Plutonium\1"
 set "destination1=%LocalAppdata%\Plutonium\bin"
-set "destination2=ur_game_folder_location"
+set "destination2=your_mw3_folder"
+set "destination3=your_bo1_folder"
 set "launcher=%LocalAppdata%\Plutonium\bin\plutonium-launcher-win32.exe"
 
-
-if not exist "%source%" (
-    echo %LocalAppdata%\Plutonium\1 does not exist
-    exit /b
-)
-
-
-if not exist "%destination1%" (
-    echo the location %destination1% does not exist
-    
-)
-
-
-if not exist "%destination2%" (
-    echo the location destination2 %destination2% does not exist
-)
-
-
 xcopy /s /i /y "%source%\*" "%destination1%"
-
-
 xcopy /s /i /y "%source%\*" "%destination2%"
+xcopy /s /i /y "%source%\*" "%destination3%"
 
 :: Lancer l'exécutable
 start "" "%launcher%"
